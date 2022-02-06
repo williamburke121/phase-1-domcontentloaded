547606e7c5baac2e02aab543fe35a626c518579f
William Warner Burke <williamburke121@gmail.com>
2022-02-05 20:29:34 -0500

    commit tree b2efd1d3c2cd93a4b45db993994163857d5df692

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    document.querySelector('p').textContent = "This is really cool!"
+  });
